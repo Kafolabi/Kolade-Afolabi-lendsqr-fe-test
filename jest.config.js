e11525252b97c64cd56c29b2811e1784c9cffc2a
Kafolabi <koladeafolabi2020@gmail.com>
2025-09-22 22:27:03 +0100

    Initialize project with vite, react, TS; and setup file structure

---
+++ jest.config.js
@@ -0,0 +1,27 @@
+// const { createDefaultPreset } = require("ts-jest");
+
+// const tsJestTransformCfg = createDefaultPreset().transform;
+
+// /** @type {import("jest").Config} **/
+// export default {
+//   testEnvironment: "node",
+//   transform: {
+//     ...tsJestTransformCfg,
+//   },
+// };
+
+const { createDefaultPreset } = require("ts-jest");
+
+const tsJestTransformCfg = createDefaultPreset().transform;
+
+/** @type {import("jest").Config} **/
+export default {
+  testEnvironment: "jsdom", // <-- use jsdom for React
+  transform: {
+    ...tsJestTransformCfg,
+  },
+  moduleNameMapper: {
+    '\\.(css|scss)$': 'identity-obj-proxy', // <-- mock CSS/SCSS imports
+  },
+  setupFilesAfterEnv: ['<rootDir>/src/setupTests.ts'], // <-- setup Testing Library
+};
